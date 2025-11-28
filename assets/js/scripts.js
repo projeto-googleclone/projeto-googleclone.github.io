278e02d4f2d182edc99a9dfe062266b3705f9d99
Wolf30k <srmystery07contac@gmail.com>
2025-11-27 22:46:30 -0300

    init

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+const searchlabs = document.querySelector("#btn-search");
+
+searchlabs.addEventListener("click", () => {
+    console.log("Clicou")
+
+})
+
+const btsorte = document.querySelector('form [type="button"]');
+
+btsorte.addEventListener("click", () => {
+
+    location.href = "https://class.hcode.com.br"
+})
